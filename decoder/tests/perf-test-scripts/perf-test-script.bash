d4184650d38b7a4bc1ae6d5d48be9fe9dbcb51e0
yuxiangyang <yuxiangyang326@gmail.com>
2023-11-20 14:07:08 +0800

    fix file path error in HOWTO.md
    cs-trace-disasm.py is pushed to linux kernel repo, and file
    path is tools/perf/scripts/python/arm-cs-trace-disasm.py.
    
    Signed-off-by: yuxiangyang <yuxiangyang326@gmail.com>

--- decoder/tests/perf-test-scripts/perf-test-script.bash
+++ decoder/tests/perf-test-scripts/perf-test-script.bash
@@ -8,4 +8,4 @@
 # run from directory containing perf.data file.
 #
 
-${PERF_EXEC_PATH}/perf --exec-path=${PERF_EXEC_PATH} script --script=python:${PERF_SCRIPT_PATH}/cs-trace-disasm.py -- -d ${XTOOLS_PATH}/aarch64-linux-gnu-objdump $*
+${PERF_EXEC_PATH}/perf --exec-path=${PERF_EXEC_PATH} script --script=python:${PERF_SCRIPT_PATH}/arm-cs-trace-disasm.py -- -d ${XTOOLS_PATH}/aarch64-linux-gnu-objdump $*
