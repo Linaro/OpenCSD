7e2f0b95469f09c0c83af729fb5243a1a86d7c0a
Ashwin Vinoo <ashwin.vinoo@ashling.com>
2025-06-13 13:16:21 +0100

    Removing file row counts as it is no longer used according to trace team. Has not been removed in default decoding format case yet.

--- decoder/source/lib_opencsd_interface.cpp
+++ decoder/source/lib_opencsd_interface.cpp
@@ -1830,21 +1830,6 @@ ocsd_datapath_resp_t TraceLogger::TraceElemIn(const ocsd_trc_index_t index_sop,
         szTemp = oss.str();
         // Prints the line to the bin file
         fprintf(m_fp_decode_out, szTemp.c_str());
-        // Increase the count of number of lines added to the file
-        m_rows_in_file++;
-        // Check if the count exceeds the maximum number of rows allowed in a files
-        // when we are to split the output into multiple files
-        if (m_rows_in_file >= m_max_rows_in_file && m_split_files)
-        {
-            // Close the current output txt file
-            CloseLogFile();
-            // Reset the row count in the txt file
-            m_rows_in_file = 0;
-            // Increase the file count
-            m_file_cnt++;
-            // Reopen a new file which will have the file count number appended to its name
-            OpenLogFile();
-        }
     }
 
     return resp;
